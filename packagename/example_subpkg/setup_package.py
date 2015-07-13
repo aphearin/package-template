884575b9529cfc8518ce71c99d680cb023c16e46
Andrew Hearin <andrew.hearin@yale.edu>
2015-07-13 17:56:39 -0400

    Changed names argument in Extension call

--- packagename/example_subpkg/setup_package.py
+++ packagename/example_subpkg/setup_package.py
@@ -9,7 +9,7 @@ ROOT = os.path.relpath(os.path.dirname(__file__))
 def get_extensions():
     sources = [os.path.join(ROOT, 'simple_cython_wrapper.pyx')]
     ext = Extension(
-        name="packagename.example_subpkg.minimal_cext",
+        name="packagename.example_subpkg.simple_cython_wrapper",
         sources=sources)
     return [ext]
 
